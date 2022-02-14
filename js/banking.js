4ee119e812cc7fb40bc0073299362cbfe792137b
arifphero <ahammadarif232@gmail.com>
2022-02-14 13:55:14 +0600

    update balance with withdraw input

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,4 @@
+// handle deposite button
 document
   .getElementById("deposit-button")
   .addEventListener("click", function () {
@@ -12,8 +13,42 @@ document
     const previousDepositTotal = parseFloat(depositTotalText);
 
     depositTotal.innerText = previousDepositTotal + depositAmount;
-    console.log(depositTotalText);
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
 
     // clear input field
     depositInput.value = "";
+  });
+
+//   handle withdraw button
+document
+  .getElementById("withdraw-button")
+  .addEventListener("click", function () {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    // update withdraw total
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+    //  clear input field
+    withdrawInput.value = "";
   });
