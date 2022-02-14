b8baffc36ff1ce4987058fb0b27d7f3ad2b51f31
arifphero <ahammadarif232@gmail.com>
2022-02-14 13:27:53 +0600

    deposite amount set

---
+++ js/banking.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("deposit-button")
+  .addEventListener("click", function () {
+    const depositInput = document.getElementById("deposit-input");
+
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+
+    //get current deposit
+    const depositTotal = document.getElementById("deposit-total");
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+    console.log(depositTotalText);
+
+    // clear input field
+    depositInput.value = "";
+  });
